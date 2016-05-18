14436b28bd6860f671910ba5c0ac67f2a267b050
jpdwalsh <jpd.walsh@gmail.com>
2016-05-18 15:04:53 +0100

    Create HelloWorld.java
    Creating Java source file,  HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+* hello world class implement a app that prints "hello World"
+*/
+
+class HelloWorld {
+  public static void main (String args) {
+    System.out.println("Hello World");
+  }
+}
